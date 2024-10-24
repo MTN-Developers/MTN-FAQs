33393b4f8970745750e2b621642bcfb2c69dec91
Mazin Islam <zamzam2960@gmail.com>
2024-10-24 10:39:20 +0300

    Refactor 404 page styling

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,47 +1,30 @@
-"use client";
-import React from "react";
-import notFoundBanner from "@/app/assets/images/notFound.svg";
-import starsOverlay from "@/app/assets/images/stars.svg";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
+'use client';
+import React from 'react';
+import notFoundBanner from '@/app/assets/images/notFound.svg';
+import starsOverlay from '@/app/assets/images/stars.svg';
+import Image from 'next/image';
 
 const Page = () => {
-  const router = useRouter();
-
-  return (
-    <div className="relative mt-10 w-full h-[400px] overflow-hidden">
-      <Image
-        src={notFoundBanner}
-        alt="Not Found"
-        fill
-        style={{ objectFit: "contain", zIndex: 1 }}
-      />
-      <Image
-        src={starsOverlay}
-        alt="Stars Overlay"
-        fill
-        style={{ objectFit: "cover" }}
-      />
-      <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
-        <p className="text-4xl font-bold">OOPS!</p>
-        <p className="text-2xl mt-2">Page not found</p>
-        <div className="mt-6 flex space-x-4">
-          <button
-            onClick={() => router.push("/")}
-            className="px-4 py-2 bg-blue-600 hover:bg-blue-700 rounded"
-          >
-            GO HOME
-          </button>
-          <button
-            onClick={() => router.back()}
-            className="px-4 py-2 bg-gray-600 hover:bg-gray-700 rounded"
-          >
-            GO BACK
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='w-full h-screen overflow-hidden flex flex-col items-center justify-end'>
+			<Image
+				src={notFoundBanner}
+				alt='Not Found'
+				fill
+				style={{ objectFit: 'contain', zIndex: 0 }}
+			/>
+			<Image
+				src={starsOverlay}
+				alt='Stars Overlay'
+				fill
+				style={{ objectFit: 'cover', zIndex: 1 }}
+			/>
+			<div className='flex flex-col items-center gap-y-2 text-white mb-20 font-light font-pnu'>
+				<p className='text-4xl'>OOPS!</p>
+				<p className='text-3xl uppercase -mt-8'>Page not found</p>
+			</div>
+		</div>
+	);
 };
 
 export default Page;
