080992d077a99a7ae49533b0908b616761f9c874
Mazin Islam <zamzam2960@gmail.com>
2024-10-24 11:57:26 +0300

    Refactor page.tsx to remove unused imports and extract getCourseData function

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,10 +1,9 @@
-import { Metadata } from 'next';
 import FaqPageClient from '../components/FaqPageClient';
-import { CourseMetaData, GetFaqsResponse } from '../types';
 import { Suspense } from 'react';
 import Loader from '../components/ui/Loader';
 import { notFound } from 'next/navigation';
 import Header from '../components/Header';
+import { getCourseData } from '../utils/getCourseData';
 
 export async function generateStaticParams() {
 	// Fetch all slugs at build time
@@ -13,63 +12,6 @@ export async function generateStaticParams() {
 	const slugs = slugsData.map((slugObj: { slug: string }) => ({ slug: slugObj.slug }));
 
 	return slugs;
-}
-
-async function getCourseData(slug: string) {
-	// Fetch the Course ID and FAQ data on the server side
-	const courseMetaDataResponse = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/course_meta_data/slug/${slug}`);
-
-	if (!courseMetaDataResponse.ok) {
-		throw new Error(`HTTP error fetching course ID! status: ${courseMetaDataResponse.status}`);
-	}
-
-	const courseMetaData: CourseMetaData = await courseMetaDataResponse.json();
-	const courseMetaDataId = courseMetaData.id;
-
-	const faqsResponse = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/course_faqs/${courseMetaDataId}?limit=10000`);
-
-	if (!faqsResponse.ok) {
-		throw new Error(`HTTP error fetching FAQs! status: ${faqsResponse.status}`);
-	}
-
-	const faqsData: GetFaqsResponse = await faqsResponse.json();
-	return {
-		course: courseMetaData,
-		faqsData: faqsData,
-	};
-}
-
-export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
-	// Fetch course data based on slug
-	try {
-		const { course, faqsData } = await getCourseData(params.slug);
-
-		return {
-			title: `${course.course_name_ar} | ${course.course_name_en}`,
-			description: course.course_name_ar,
-			keywords: faqsData.items.map((faq) => faq.title).join(', '),
-			robots: 'index, follow',
-			openGraph: {
-				title: `${course.course_name_ar} | ${course.course_name_en}`,
-				description: course.course_name_ar,
-				images: [
-					{
-						url: course.course_logo,
-						width: 800,
-						height: 600,
-						alt: course.course_name_ar,
-					},
-				],
-			},
-		};
-	} catch (error) {
-		console.error('Error fetching course data:', error);
-		return {
-			title: '404 | Not Found',
-			description: 'This course does not exist',
-			keywords: 'This course does not exist',
-		};
-	}
 }
 
 const Page = async ({ params }: { params: { slug: string } }) => {
