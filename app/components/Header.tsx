6e797f2e870b31760d379ecc45e6dcb43fb0cbfc
Mazin Islam <zamzam2960@gmail.com>
2024-10-24 10:39:01 +0300

    Refactor Header to get logo from the course meta data

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,26 +1,25 @@
-"use client";
-import React from "react";
-import logo from "@/app/assets/images/logo.svg";
-import Image from "next/image";
-import { useParams } from "next/navigation";
+'use client';
+import React from 'react';
+import Image from 'next/image';
+import { useParams } from 'next/navigation';
 
-const Header = () => {
-  const params = useParams();
-  const slug = params.slug;
+const Header = ({ logo }: { logo: string }) => {
+	const params = useParams();
+	const slug = params.slug;
 
-  return (
-    <div className="w-full">
-      <a href={`/${slug}`}>
-        <Image
-          src={logo}
-          alt="logo"
-          width={200}
-          height={70}
-          className="md:pt-[100px] mx-auto mt-10 cursor-pointer"
-        />
-      </a>
-    </div>
-  );
+	return (
+		<div className='w-full'>
+			<a href={`/${slug}`}>
+				<Image
+					src={logo}
+					alt='logo'
+					width={200}
+					height={70}
+					className='md:pt-[100px] mx-auto mt-10 cursor-pointer'
+				/>
+			</a>
+		</div>
+	);
 };
 
 export default Header;
