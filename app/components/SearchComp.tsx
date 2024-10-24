92a01b23c309716fd8f4ab3079e9597e9a5f0d22
Mazin Islam <zamzam2960@gmail.com>
2024-10-24 10:39:39 +0300

    changed search comp to rtl

--- app/components/SearchComp.tsx
+++ app/components/SearchComp.tsx
@@ -1,43 +1,42 @@
-"use client";
+'use client';
 
-import React, { useState } from "react";
-import { Input } from "antd";
-import { useParams, useRouter } from "next/navigation";
+import React, { useState } from 'react';
+import { Input } from 'antd';
+import { useParams, useRouter } from 'next/navigation';
 
 const { Search } = Input;
 
 const SearchComp = () => {
-  const params = useParams();
-  const slug = params.slug;
+	const params = useParams();
+	const slug = params.slug;
 
-  // console.log(typeof slug);
+	const [searchValue, setSearchValue] = useState('');
+	const router = useRouter();
 
-  const [searchValue, setSearchValue] = useState("");
-  const router = useRouter();
+	const onSearch = (value: string) => {
+		if (value.trim() !== '') {
+			if (slug) {
+				router.push(`/${slug}/search?query=${encodeURIComponent(value)}`);
+			} else {
+				router.push(`/${slug}/search?query=${encodeURIComponent(value)}`);
+			}
+		}
+	};
 
-  const onSearch = (value: string) => {
-    if (value.trim() !== "") {
-      if (slug) {
-        router.push(`/${slug}/search?query=${encodeURIComponent(value)}`);
-      } else {
-        router.push(`/${slug}/search?query=${encodeURIComponent(value)}`);
-      }
-    }
-  };
-
-  return (
-    <div className="h-full w-full flex">
-      <Search
-        placeholder="ابحث هنا" // "Search here" in Arabic
-        allowClear
-        onSearch={onSearch}
-        value={searchValue}
-        onChange={(e) => setSearchValue(e.target.value)}
-        className="flex-grow h-full"
-        style={{ height: "100%" }}
-      />
-    </div>
-  );
+	return (
+		<div className='h-full w-full flex'>
+			<Search
+				placeholder='ابحث هنا' // "Search here" in Arabic
+				allowClear
+				onSearch={onSearch}
+				value={searchValue}
+				onChange={(e) => setSearchValue(e.target.value)}
+				className='flex-grow h-full'
+				style={{ height: '100%' }}
+				dir='rtl'
+			/>
+		</div>
+	);
 };
 
 export default SearchComp;
