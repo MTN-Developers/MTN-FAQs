20498730bd560700eea87f9d8a2cd996f39d398d
Mazin Islam <zamzam2960@gmail.com>
2024-10-24 10:40:09 +0300

    Refactor page component to add loader for search results and handle 404 errors

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,6 +1,10 @@
 import { Metadata } from 'next';
 import FaqPageClient from '../components/FaqPageClient';
 import { CourseMetaData, GetFaqsResponse } from '../types';
+import { Suspense } from 'react';
+import Loader from '../components/ui/Loader';
+import { notFound } from 'next/navigation';
+import Header from '../components/Header';
 
 export async function generateStaticParams() {
 	// Fetch all slugs at build time
@@ -37,42 +41,58 @@ async function getCourseData(slug: string) {
 
 export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
 	// Fetch course data based on slug
-	const { course, faqsData } = await getCourseData(params.slug);
+	try {
+		const { course, faqsData } = await getCourseData(params.slug);
 
-	return {
-		title: `${course.course_name_ar} | ${course.course_name_en}`,
-		description: course.course_name_ar,
-		keywords: faqsData.items.map((faq) => faq.title).join(', '),
-		robots: 'index, follow',
-		viewport: 'width=device-width, initial-scale=1',
-		openGraph: {
+		return {
 			title: `${course.course_name_ar} | ${course.course_name_en}`,
 			description: course.course_name_ar,
-			images: [
-				{
-					url: course.course_logo,
-					width: 800,
-					height: 600,
-					alt: course.course_name_ar,
-				},
-			],
-		},
-	};
+			keywords: faqsData.items.map((faq) => faq.title).join(', '),
+			robots: 'index, follow',
+			openGraph: {
+				title: `${course.course_name_ar} | ${course.course_name_en}`,
+				description: course.course_name_ar,
+				images: [
+					{
+						url: course.course_logo,
+						width: 800,
+						height: 600,
+						alt: course.course_name_ar,
+					},
+				],
+			},
+		};
+	} catch (error) {
+		console.error('Error fetching course data:', error);
+		return {
+			title: '404 | Not Found',
+			description: 'This course does not exist',
+			keywords: 'This course does not exist',
+		};
+	}
 }
 
 const Page = async ({ params }: { params: { slug: string } }) => {
 	const { slug } = params;
 
 	// Fetch course data on the server
-	const { course, faqsData } = await getCourseData(slug);
+	try {
+		const { course, faqsData } = await getCourseData(slug);
 
-	return (
-		<FaqPageClient
-			slug={slug}
-			courseId={course.id}
-			faqsData={faqsData.items}
-		/>
-	);
+		return (
+			<Suspense fallback={<Loader />}>
+				<Header logo={course.course_logo} />
+				<FaqPageClient
+					slug={slug}
+					courseId={course.id}
+					faqsData={faqsData.items}
+				/>
+			</Suspense>
+		);
+	} catch (error) {
+		console.error(error);
+		notFound();
+	}
 };
 
 export default Page;
