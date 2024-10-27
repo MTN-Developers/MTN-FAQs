c5bfb2d25f65d5e7e5ab678e26fa75ef17b2d9f1
Mazin Islam <zamzam2960@gmail.com>
2024-10-27 16:06:53 +0300

    Refactor next.config.mjs to update remotePatterns for image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ['s3.amazonaws.com'],
+		remotePatterns: [
+			{
+				hostname: 's3.amazonaws.com',
+			},
+		],
 	},
 };
 
